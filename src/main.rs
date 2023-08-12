2c9458d9c5982631addb46cf303be0f74b8c1b22
Johan Liebert <bggbss@rambler.ru>
2023-08-12 03:36:27 +0300

    Project "Number devision by 2023?"

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
-fn main() {
+fn main() { //? Number devision by 2023?
+    let mut number:u128 = 0;
+    let mut counter:u8 = 0;
+    
+    // * There is no efficient overshoot
     loop {
-        let text = String::from("Again!");
-        println!("{text}");
+        number+=1;
+        if number % 2023 == 0 {
+            println!("{number}");
+            counter+=1;
+            if counter == 10 {break};
+        }
+
     }
 }
