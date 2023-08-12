54c04ebf81d48aac758cb7a4057d740e153796e9
Johan Liebert <bggbss@rambler.ru>
2023-08-12 04:18:14 +0300

    Optimized array check

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
-fn main() {
+fn main() { 
     let array: [u8; 5] = [10, 20, 30, 40, 50];
-    let mut index = 0;
-
-    while index < array.len() {
-        println!("Array index {index} = {}", array[index]);
-        index += 1;
-        
+    for elements in array { //* Optimized check
+        println!("{elements}")
     }
 
 }
